acf1d02af93cc3e5bc37967131a4ea2bd589e94e
root <root@localhost.localdomain>
2017-11-13 00:24:29 +0530

    Third-commit

--- First-Angular-Project/src/app/components/user/user.component.ts
+++ First-Angular-Project/src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import{ DataService } from '../../services/data.service'
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html',
@@ -10,8 +10,7 @@ export class UserComponent implements OnInit {
 
     name: string;
     age: number;
-
-email: string;
+    email: string;
     address: Address;
     hobbies: string[];
     hello: any;
@@ -19,12 +18,49 @@ email: string;
     isEdit: boolean = false;
 
 
-    constructor() {
+    constructor(private dataService:DataService) {
         console.log('constructor ran..');
     }
 
     ngOnInit() {
+        console.log('ngOnInit ran...');
+
+        this.name = 'John Doe';
+        this.email = 'test@test.com';
+        this.age = 30;
+        this.address = {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
+        }
+        this.hobbies = ['Write code', 'Watch movies', 'Listen to music'];
+
+        this.dataService.getPosts().subscribe((posts) => {
+            //console.log(posts);
+            this.posts = posts;
+        });
+
     }
+    onClick() {
+        this.name = 'Brad Traversy';
+        this.hobbies.push('New Hobby');
+    }
+
+    addHobby(hobby) {
+        console.log(hobby);
+        this.hobbies.unshift(hobby);
+        return false;
+    }
+
+    deleteHobby(i) {
+        this.hobbies.splice(i, 1);
+    }
+
+    toggleEdit() {
+        this.isEdit = !this.isEdit;
+    }
+
+
 
 }
 
