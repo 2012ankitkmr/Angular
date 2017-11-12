318f571f3b66b98c7dfd557a8a37ba5872b88c78
root <root@localhost.localdomain>
2017-11-12 23:24:20 +0530

    second commit

--- First-Angular-Project/src/app/components/user/user.component.ts
+++ First-Angular-Project/src/app/components/user/user.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+    name: string;
+    age: number;
 
-  ngOnInit() {
-  }
+email: string;
+    address: Address;
+    hobbies: string[];
+    hello: any;
+    posts: Post[];
+    isEdit: boolean = false;
 
+
+    constructor() {
+        console.log('constructor ran..');
+    }
+
+    ngOnInit() {
+    }
+
+}
+
+
+interface Address {
+    street: string,
+    city: string,
+    state: string
+}
+
+interface Post {
+    id: number,
+    title: string,
+    body: string,
+    userId: number
 }
