94a1658c2d2a3a5b9ca1f7230f442f30de421747
root <root@localhost.localdomain>
2017-11-19 20:18:47 +0530

    modified

--- Projectprimeng/src/app/services/user.service.ts
+++ Projectprimeng/src/app/services/user.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/throw';
+
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+    private baseURL: string = 'localhost:8080/api';
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+    private options = new RequestOptions({ headers: this.headers });
+
+
+    constructor(private _http: Http) { }
+
+    getUsers() {
+
+        return this._http.get(this.baseURL + '/users', this.options).map((response: Response) => response.json())
+            .catch(this.errorHandler);
+    }
+
+    errorHandler(error: Response) {
+        return
+    }
 
 }
